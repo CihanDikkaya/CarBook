1baa2e4805dc54a512b737c29960489a8e5d2f4d
Cihan <cihandikkaya09@hotmail.com>
2025-12-15 16:37:50 +0300

    MediatR tasarım düzeni...

---
+++ Core/CarBook.Application/Mediator/Handlers/FeatureHandlers/GetFeatureQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using CarBook.Application.Features.CQRS.Results.BrandResults;
+using CarBook.Application.Interfaces;
+using CarBook.Application.Mediator.Queries.FeatureQueries;
+using CarBook.Application.Mediator.Results.FeatureResults;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Mediator.Handlers.FeatureHandlers
+{
+    public class GetFeatureQueryHandler : IRequestHandler<GetFeatureQuery, List<GetFeatureQueryResult>>
+    {
+        private readonly IRepository<Feature> _repository;
+
+        public GetFeatureQueryHandler(IRepository<Feature> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<List<GetFeatureQueryResult>> Handle(GetFeatureQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetAllAsync();
+            return values.Select(f => new GetFeatureQueryResult
+            {
+                FeatureID = f.FeatureID,
+                FeatureName = f.FeatureName
+            }).ToList();
+
+        }
+    }
+}
+
---
+++ Core/CarBook.Application/Mediator/Handlers/FeatureHandlers/UpdateFeatureCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Application.Interfaces;
+using CarBook.Application.Mediator.Commands.FeatureCommands;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Mediator.Handlers.FeatureHandlers
+{
+    public class UpdateFeatureCommandHandler : IRequestHandler<UpdateFeatureCommand>
+    {
+        private readonly IRepository<Feature> _repository;
+
+        public UpdateFeatureCommandHandler(IRepository<Feature> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task Handle(UpdateFeatureCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _repository.GetByIdAsync(request.FeatureID);
+            values.FeatureName = request.FeatureName;
+            await _repository.UpdateAsync(values);
+        }
+    }
+}
